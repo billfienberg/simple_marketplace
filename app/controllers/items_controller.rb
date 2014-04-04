e998420124313a77e7d8a98e44a35817224d9f01
Todd Wacker <twac05@yahoo.com>
2014-04-04 15:28:43 -0500

    Link tags added to individual item view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.all
+    @item = Item.find(params[:id])
   end
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,5 +1,5 @@
 class Seller < ActiveRecord::Base
 	has_many :transactions
-	has_many :buyers, through: :transactions
+	has_one :buyer, through: :transactions
 	has_many :items
 end
